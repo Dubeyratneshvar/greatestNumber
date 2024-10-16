566c1f45985137156b4e9052177ef76ba4a61769
Ratneshvar Dubey <130789543+Dubeyratneshvar@users.noreply.github.com>
2024-10-16 22:27:18 +0530

    Update greatestNumber.java

--- greatestNumber.java
+++ greatestNumber.java
@@ -20,11 +20,11 @@ public class greatestNumber {
         c = sc.nextInt();
 
         if (a > b && a > c) {
-            System.out.println("A is greatest number");
+            System.out.println(a + " is greatest number");
         } else if (b > a && b > c) {
-            System.out.println("B is greatest number");
+            System.out.println(b + "is greatest number");
         } else {
-            System.out.println("C is greatest number");
+            System.out.println(c + "C is greatest number");
         }
     }
 }
